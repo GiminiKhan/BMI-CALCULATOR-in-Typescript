e1653d531550da98e427b94879689737b76fe06d
GiminiKhan <160707616+GiminiKhan@users.noreply.github.com>
2024-03-14 08:52:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// BMI CALCULATOR in Typescript
+var weightInKg = 70;
+var heightInMeters = 1.78;
+var bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log("Qurratulain your BMI is ".concat(bmi));
---
+++ main.ts
@@ -0,0 +1,8 @@
+// BMI CALCULATOR in Typescript
+
+let weightInKg = 70;
+let heightInMeters = 1.78;
+
+let bmi = weightInKg/ (heightInMeters * heightInMeters)
+
+console.log(`Qurratulain your BMI is ${bmi}`)
